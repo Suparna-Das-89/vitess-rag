000a907146bc4164271218fa1ee1dfcd5c94543a
Suparna Das <suparna.math@gmail.com>
2026-05-08 14:17:30 +0200

    Add imports and __all__ definition in __init__.py

--- src/vitess_rag/__init__.py
+++ src/vitess_rag/__init__.py
@@ -1,1 +1,20 @@
+from .agent import ask_hybrid_agent, build_vitess_research_agent
+from .chroma import (
+    build_chunks_from_directory,
+    chunks_to_chroma_inputs,
+    get_or_create_collection,
+    index_markdown_directory,
+)
+from .embeddings import BlabladorEmbeddingFunction
+from .parser import parse_markdown_document
 
+__all__ = [
+    "BlabladorEmbeddingFunction",
+    "ask_hybrid_agent",
+    "build_chunks_from_directory",
+    "build_vitess_research_agent",
+    "chunks_to_chroma_inputs",
+    "get_or_create_collection",
+    "index_markdown_directory",
+    "parse_markdown_document",
+]
